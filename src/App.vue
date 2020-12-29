8954c53578005bccdc033a927a6c7212b128878f
unknown <tsitsilinden@yandex.ru>
2020-12-30 02:35:31 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card text-center m-3">
+    <table>
+      <tr>
+        <th>Регион</th>
+        <th>Заражения</th>
+        <th>Выздоровления</th>
+        <th>Смерти</th>
+      </tr>
+      <tr v-for="item in totalVuePackages.russia_stat_struct.data" :key="item.info.name">
+        <td>{{item.info.short_name}}</td>
+        <td>{{item.info.cases}}   <sup><small>+{{item.info.cases_delta}}</small></sup></td>
+        <td>{{item.info.cured}}   <sup><small>+{{item.info.cured_delta}}</small></sup></td>
+        <td>{{item.info.deaths}}  <sup><small>+{{item.info.deaths_delta}}</small></sup></td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "get-request",
+    data() {
+      return {
+        totalVuePackages: null
+      };
+    },
+    created() {
+      // Simple GET request using axios
+      axios.get("https://yastat.net/s3/milab/2020/covid19-stat/data/v3/default_data.json")
+              .then(response => this.totalVuePackages = response.data);
+    }
+  };
 </script>
 
 <style>
